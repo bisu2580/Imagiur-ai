ca5f21edb7a41c489ec5e3a496ecf8dc20313377
Biswajit Sahoo <biswatian123@gmail.com>
2025-09-07 13:33:36 +0530

    changed server file

--- backend/server.js
+++ backend/server.js
@@ -36,5 +36,5 @@ app.get(/.*/, (req, res) => {
   res.sendFile(path.join(frontendDistPath, "index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
